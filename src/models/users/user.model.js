be0d4b087c1247d390ac961a856a56657133b9fb
Phong Nguyen Dinh <phongnd1203@proton.me>
2024-03-05 12:16:56 +0700

    Add user model

---
+++ src/models/users/user.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const UserModel = model("User", userSchema);
+
+export default UserModel;
