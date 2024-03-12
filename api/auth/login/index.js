513094435cff02bfed7a20ef90ec9c020d5a3507
Phong Nguyen Dinh <phongnd1203@proton.me>
2024-03-12 20:02:27 +0700

    Migrate to serverless function

--- api/auth/login/index.js
+++ api/auth/login/index.js
@@ -1,10 +1,14 @@
-const argon2 = require("argon2");
 const express = require("express");
+
 const validator = require("express-validator");
 
-const UserModel = require("../../models/users/user.model");
+const argon2 = require("argon2");
+
+const UserModel = require("../../../models/users/user.model");
 
 const loginRouter = express.Router();
+
+loginRouter.path = "/login";
 
 loginRouter.get("/", async (req, res) => {
   res.render("auth/login.view.ejs");
@@ -22,26 +26,29 @@ loginRouter.post(
     .isLength({ min: 8 })
     .withMessage("Mật khẩu phải chứa ít nhất 8 ký tự"),
   async (req, res) => {
-    // Data validation
     const validationErrors = validator.validationResult(req);
+
     if (!validationErrors.isEmpty()) {
       return res.status(400).render("auth/login.view.ejs", {
         errors: validationErrors.array(),
       });
     }
 
-    // User authentication
+    const { email, password } = req.body;
 
-    const user = await UserModel.findOne({ email }).select("+password").exec();
+    const user = await UserModel.findOne({ email })
+      .select({ password: 1 })
+      .lean()
+      .exec();
 
-    if (!user) {
+    if (!user || !(await argon2.verify(user.password, password))) {
       return res.status(400).render("auth/login.view.ejs", {
-        errors: [{ msg: "Email không tồn tại" }],
+        errors: [{ msg: "Email hoặc mật khẩu không chính xác" }],
       });
     }
-    // Session handling
 
-    // Redirect to home page
+    // TODO: Handle user session
+
     res.redirect("/");
   },
 );
--- api/index.js
+++ api/index.js
@@ -1,3 +1,66 @@
-const app = require("../src/app.js");
+const path = require("path");
+const glob = require("glob");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("express-session");
+
+const MongoStore = require("connect-mongo");
+const mongodbConnection = require("../databases/init.mongodb.js");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+app.use(express.static(path.join(__dirname, "../static")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "sid",
+    secret: process.env.APP_SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store: MongoStore.create({
+      client: mongodbConnection.getClient(),
+      collectionName: "sessions",
+      ttl: 60 * 60 * 24 * 14,
+      autoRemove: "native",
+      autoRemoveInterval: 10,
+      crypto: {
+        secret:
+          process.env.MONGODB_SESSION_SECRET || process.env.APP_SESSION_SECRET,
+      },
+    }),
+  }),
+);
+
+const routes = glob.sync(`${__dirname}/**/index.js`, {
+  ignore: [`${__dirname}/index.js`],
+});
+routes.forEach((route) => {
+  const router = require(route);
+  const routePath =
+    router.path || `/${path.dirname(path.relative(__dirname, route))}`;
+  app.use(routePath, router);
+});
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+app.on("close", async () => {
+  await mongodbConnection.close();
+});
 
 module.exports = app;
