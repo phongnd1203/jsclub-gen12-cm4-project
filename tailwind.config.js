a12ea40fdb8021b8d876c6cde2e1d54a5167d07c
Phong Nguyen Dinh <phongnd1203@proton.me>
2024-03-17 14:27:24 +0700

    Add home featured view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["views/**/*.ejs"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       screens: {
         landscape: { raw: "(orientation: landscape)" },
