76f96a6b269e4ddf9460f97e056d55b28850fc53
Phong Nguyen Dinh <phongnd1203@proton.me>
2024-03-17 11:01:02 +0700

    Add dist folder to generated static file serving

--- api/index.js
+++ api/index.js
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
