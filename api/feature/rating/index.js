2b7368c9b5e405789460c0f9ef19fbf007d12d20
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-03-20 14:07:05 +0700

    processing

--- api/feature/rating/index.js
+++ api/feature/rating/index.js
@@ -1,69 +1,12 @@
-const HouseModel = require('./path/to/HouseModel'); // Import your House model
+const express = require('express');
+const ratingRouter = express.Router();
+const UserModel = require('../../../models/users/user.model');
+const ratingService = require('../../../services/features/rating.service');
 
-// CREATE: Thêm một đánh giá mới
-const addRating = async (houseId, userId, stars) => {
-  try {
-    const house = await HouseModel.findById(houseId);
-    if (!house) {
-      throw new Error("House not found");
-    }
+ratingRouter.post('/rate/create', ratingService.addRating);
 
-    house.ratings.push({ user: userId, stars });
-    await house.save();
-    return house;
-  } catch (error) {
-    throw error;
-  }
-};
+ratingRouter.post('/rate/update', ratingService.updateRating);
 
-// READ: Lấy tất cả đánh giá của một căn nhà
-const getRatings = async (houseId) => {
-  try {
-    const house = await HouseModel.findById(houseId);
-    if (!house) {
-      throw new Error("House not found");
-    }
-    return house.ratings;
-  } catch (error) {
-    throw error;
-  }
-};
+ratingRouter.post('/rate/delete', ratingService.deleteRating);
 
-// UPDATE: Cập nhật đánh giá của một người dùng
-const updateRating = async (houseId, userId, stars) => {
-  try {
-    const house = await HouseModel.findById(houseId);
-    if (!house) {
-      throw new Error("House not found");
-    }
-
-    const rating = house.ratings.find(rating => rating.user.equals(userId));
-    if (!rating) {
-      throw new Error("Rating not found");
-    }
-
-    rating.stars = stars;
-    await house.save();
-    return house;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// DELETE: Xóa đánh giá của một người dùng
-const deleteRating = async (houseId, userId) => {
-  try {
-    const house = await HouseModel.findById(houseId);
-    if (!house) {
-      throw new Error("House not found");
-    }
-
-    house.ratings = house.ratings.filter(rating => !rating.user.equals(userId));
-    await house.save();
-    return house;
-  } catch (error) {
-    throw error;
-  }
-};
-
-module.exports = { addRating, getRatings, updateRating, deleteRating };
+module.exports = ratingRouter;
