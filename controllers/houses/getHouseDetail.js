757490861411b2bc1df19eb2e456d803506bd086
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-03-22 22:04:28 +0700

    change icon heart favorite FrontEnd

--- controllers/houses/getHouseDetail.js
+++ controllers/houses/getHouseDetail.js
@@ -3,18 +3,19 @@ const { StatusCodes } = require("http-status-codes");
 const HttpException = require("../../utils/httpException.js");
 
 const getHousesService = require("../../services/houses/getHouses.js");
+const favoriteService = require('../../services/houses/favorite/favoriteService.js')
 
 const getHouseDetailPage = async (req, res, next) => {
   try {
     const { id } = req.params;
-
+    const { userId } = req.session;
     const house = await getHousesService.getHouseById(id);
-
+    const isFavorite = await favoriteService.getFavorite(userId, id);
     if (!house) {
       throw new HttpException(StatusCodes.NOT_FOUND, "Không tìm thấy nhà");
     }
 
-    return res.render("pages/houses/detail.ejs", { house });
+    return res.render("pages/houses/detail.ejs", { house, isFavorite});
   } catch (error) {
     return next(error);
   }
